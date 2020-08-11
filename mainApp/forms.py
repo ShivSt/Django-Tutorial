bfebb579310453af9dfe65693940750715c98204
ShivSt <sachinraina07@gmail.com>
2020-08-11 17:33:41 +0530

    added custom form and able to store tutorial from frontend

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -2,10 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Tutorial, TutorialSeries, TutorialCategory
-from tinymce.widgets import TinyMCE
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=False)
 
     class Meta:
         model = User
@@ -19,7 +18,8 @@ class NewUserForm(UserCreationForm):
         return user
 
 
-class TutorialForm(forms.ModelForm):    # We extends forms.Form for custom form
+class TutorialForm(forms.Form):    # We extends forms.ModelForm for easy form
+    """
     tutorial_slug = forms.CharField(help_text="slug is a unique identifier for the tutorial")
     tutorial_content = forms.CharField(widget=TinyMCE(mce_attrs={'width': 100})
      )
@@ -28,29 +28,31 @@ class TutorialForm(forms.ModelForm):    # We extends forms.Form for custom form
         model = Tutorial
         fields = ("tutorial_title", "tutorial_series", "tutorial_slug", "tutorial_content")
         
-    """ using forms.Form
-    #all_series = [s.tutorial_series for s in TutorialSeries.objects.all()]
-    SERIES_CHOICES = [(obj.tutorial_series , obj) for obj in TutorialSeries.objects.all()]
+    """ 
+    
+    SERIES_CHOICES = [(obj , obj) for obj in TutorialSeries.objects.all()]
     title = forms.CharField()
     linked_series = forms.ChoiceField(choices=SERIES_CHOICES)
-    content = forms.CharField(widget=forms.Textarea )
-    slug= forms.CharField()
+    content = forms.CharField()
+    slug= forms.CharField(help_text='slug is a unique identifier for this tutorial')
 
     class Meta:
         model = Tutorial
         fields = ("tutorial_title", "tutorial_series", "tutorial_slug", "tutorial_content")
 
+
     def save(self, commit=True):
         
-        tutorial = Tutorial()   #super(TutorialForm, self).save(commit=False)
+        tutorial = Tutorial()  #super(TutorialForm, self)
         tutorial.tutorial_title = self.cleaned_data["title"]
-        tutorial.tutorial_series = self.cleaned_data["linked_series"] 
+        # this filtering is needed because the choiceField is returning string and we need object
+        tutorial.tutorial_series = TutorialSeries.objects.filter(tutorial_series = self.cleaned_data['linked_series'])[0]
         tutorial.tutorial_content = self.cleaned_data["content"]
         tutorial.tutorial_slug = self.cleaned_data["slug"]
         if commit:
             tutorial.save()
         return tutorial
-    """
+    
 
 class TutorialCategoryForm(forms.ModelForm):
     category_slug = forms.CharField(help_text="slug is a unique identifier for the category. It will be used for category url")
--- mainApp/urls.py
+++ mainApp/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.logout_request, name='logout'),
     path('login/', views.login_request, name='login'),
-    path('<single_slug>', views.single_slug, name='single_slug'),
     path('addTutorial/', views.add_new_tutorial, name='addTutorial'),
     path('addCategory/', views.add_new_tutorial_category, name='addCategory'),
     path('addSeries/', views.add_new_tutorial_series, name='addSeries'),
+    path('<single_slug>', views.single_slug, name='single_slug'),
 ]
