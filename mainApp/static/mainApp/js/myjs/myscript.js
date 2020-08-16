7aeda62d7f46c6bea4c31db69b41d14ea5d05ae4
ShivSt <sachinraina07@gmail.com>
2020-08-16 15:28:08 +0530

    updated app for mobile navigation and added tooltip

---
+++ mainApp/static/mainApp/js/myjs/myscript.js
@@ -0,0 +1,25 @@
+
+/* Use materializecss js functions/initializations only if you want to add any
+options other than default. As default intialization for all the materializecss
+components have done in header template with <script>M.AutoInit();</script> */
+
+// categories: Add floating button options
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var options = {
+                    direction: 'top',      //default='top'
+                    hoverEnabled: true,    //default=true
+                    //toolbarEnabled: true    //default=false
+                }
+    var instances = M.FloatingActionButton.init(elems, options);
+});
+
+// categories: Tooltip options for buttons
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    const options = {
+        position: 'left',
+    }
+    var instances = M.Tooltip.init(elems, options);
+});
+
